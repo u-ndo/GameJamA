27a73d22b0fb2993a9fde0f71f1732181ae01e17
You-Ki <You-Ki>
2017-05-04 12:29:52 +0900

    init

---
+++ Assets/Script/pants.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pants : MonoBehaviour {
+
+    public Rigidbody2D pantsBody;
+
+    // Use this for initialization
+    void Start () {
+		pantsBody =GetComponent<Rigidbody2D>();
+        pantsBody.AddForce(transform.up*10, ForceMode2D.Impulse);
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        pantsBody.AddForce(transform.right, ForceMode2D.Impulse);
+
+	}
+}

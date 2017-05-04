d20e354c92eeec566be104ea925a9649a2fc3c0a
sourimis <abe.naoki.0314@gmail.com>
2017-05-04 18:09:42 +0900

    Change Title Scene

---
+++ Assets/Script/select.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class select : MonoBehaviour {
+
+	public void SelectSelect()
+    {
+        SceneManager.LoadScene("main");
+    }
+}

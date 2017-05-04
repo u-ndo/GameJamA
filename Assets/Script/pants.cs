4ed676197d3e4bbacb22a7d84125a0a68b7e22ca
You-Ki <You-Ki>
2017-05-04 15:05:54 +0900

    げーむおーばー処理仮実装

--- Assets/Script/pants.cs
+++ Assets/Script/pants.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class pants : MonoBehaviour {
 
@@ -22,7 +23,7 @@ public class pants : MonoBehaviour {
         if(trigger.tag=="Yuka")
         {
             //げーむおーばーしょり
-
+            SceneManager.LoadScene("GameOver");
         }
     }
 }
